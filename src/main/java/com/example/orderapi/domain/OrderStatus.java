fe3014a13ba8489f1e99fb0bfdd4271a881a75cf
Thieres Barbosa <thieres.barbosa@mercadolivre.com>
2026-04-03 18:13:16 -0300

    Atualização do OrderController e OrderStatus

--- src/main/java/com/example/orderapi/domain/OrderStatus.java
+++ src/main/java/com/example/orderapi/domain/OrderStatus.java
@@ -4,6 +4,6 @@ public enum OrderStatus {
     CREATED, PAID, SENT, DELIVERED;
 
     public boolean isValidNext(OrderStatus next) {
-        return next.ordinal() > this.ordinal();
+        return next.ordinal() == this.ordinal() + 1;
     }
 }
